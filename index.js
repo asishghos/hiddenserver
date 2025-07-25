d15a229c6657247c5dcc3272c9b18ad5ead9cacd
asishghos <ccasish01@gmail.com>
2025-07-25 17:20:59 +0530

    Fix log message for MongoDB URI in server startup

--- index.js
+++ index.js
@@ -224,5 +224,5 @@ export async function extractComplementaryClothMetaData(
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
-  console.log("Mongo URI:", process.env.MONGO_URL);
+  console.log("Mongo URL:", process.env.MONGO_URL);
 });
